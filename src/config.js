055f847ac6af403b1fefcbdabae9208c122f4ecf
af <af@gleeph.net>
2020-10-27 10:45:31 +0100

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-axel2"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://q7pwwy7hrj.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_y2ObWfbhA",
+      APP_CLIENT_ID: "63ep5c4kl4qu1gaovd2kofrv4t",
+      IDENTITY_POOL_ID: "us-east-2:3992cf62-fa7e-45ee-8c2b-0e47ff7d9fbe"
+    }
+  };
